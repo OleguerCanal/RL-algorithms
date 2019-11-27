632bf5d38a33e4aecf527eab077b35438c4999ac
Federico <federico.taschin@studenti.unipd.it>
2019-11-27 17:19:33 +0100

    Value Iteration infinite horizon

--- lab_1/c.py
+++ lab_1/c.py
@@ -1,7 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from tqdm import tqdm
-from b import State
 
 map = np.ones((7, 8))
 map[0:4, 2] = 0
@@ -11,17 +10,80 @@ map[6, 4] = 0
 map[1:4, 5] = 0
 map[2, 6:8] = 0
 
-c_rewards = {"Win": 10000, "Minotaur":-10000, "Step": -1}
+class State:
+    goal = (6, 5)
+    player_actions = [(-1, 0), (1, 0), (0, -1), (0, 1), (0, 0)]
+    mino_actions = [(-1, 0), (1, 0), (0, -1), (0, 1), (0, 0)]
+
+    def __init__(self, coord=(0, 0, 6, 5)):
+        self.player = (coord[0], coord[1])
+        self.mino = (coord[2], coord[3])
+    
+    def get_coord(self):
+        return self.player[0], self.player[1], self.mino[0], self.mino[1]
+
+    def dead(self):
+        return self.player[0] == self.mino[0] and self.player[1] == self.mino[1]
+    
+    def free(self):
+        return self.player[0] == State.goal[0] and self.player[1] == State.goal[1]
+
+    def get_actions(self):
+        if self.free():
+            return []
+        if self.dead():
+            return []
+        actions = []
+        for action in State.player_actions:
+            next_pos = np.array(self.player) + np.array(action)
+            if next_pos[0] < 0 or next_pos[0] >= map.shape[0] or\
+                next_pos[1] < 0 or next_pos[1] >= map.shape[1] or\
+                map[next_pos[0], next_pos[1]] == 0 or\
+                map[self.player[0], self.player[1]] == 0:
+                continue
+            actions.append(action)
+        return actions
+
+    def get_transitions(self, action):
+        next_state = State(self.get_coord())
+        next_state.player = tuple(np.array(next_state.player) + np.array(action))
+
+        possible_states = []
+        for mino_action in State.mino_actions:
+            next_pos = np.array(next_state.mino) + np.array(mino_action)
+            if next_pos[0] < 0 or next_pos[0] >= map.shape[0] or\
+                next_pos[1] < 0 or next_pos[1] >= map.shape[1]:
+                continue
+            possible_state = State(next_state.get_coord())
+            possible_state.mino = tuple(next_pos)
+            possible_states.append(possible_state)
+
+        prob = 1./len(possible_states)
+
+        result = []
+        for a in possible_states:
+            result.append((a, prob))
+        return result
+
+    def reward(self):
+        if self.dead():
+            return 0
+        if self.free():
+            return 1
+        return 0
+
+    def __str__(self):
+        return "Player:" + str(self.player) + ", " + "Mino:" + str(self.mino)
 
 def value_iteration_infinite(value, lambd):
     max_iters = 10000
-    tol = 1e-5
+    tol = 1e-10
     cont = 0
 
     for iter in tqdm(range(max_iters)):
         new_value = np.zeros(value.shape)
         for coords, _ in np.ndenumerate(value[:, :, :, :]):
-            state = State(coords, c_rewards)
+            state = State(coords)
             actions = state.get_actions()
             if actions == []:
                 new_value[coords] = state.reward()
@@ -72,13 +134,13 @@ def generate_game(values, initial_state, deadline):
         state = transitions[np.random.choice(range(len(transitions)))][0]
     return states, "Deadline"
 
-def test_geometric(deadlines, n_games):
+def test_geometric(deadlines, n_games, lambd):
         value = np.zeros((map.shape[0], map.shape[1], map.shape[0], map.shape[1]))
         value[6, 5, :, :] = 1
         value[6, 5, 6, 5] = 0
-        value = value_iteration_infinite(value, 29./30.)
+        value = value_iteration_infinite(value, lambd)
 
-        initial_state = State(rewards=c_rewards)
+        initial_state = State()
         xp, yp, xm, ym = initial_state.get_coord()
         
         wins = []
@@ -94,15 +156,20 @@ def test_geometric(deadlines, n_games):
 
 
 if __name__ == "__main__":
-    deadlines = [16, 17, 18, 19, 20, 22, 24, 25, 30, 35, 40]
-    n_games = 100
-    wins = test_geometric(deadlines, n_games)
 
-    fig = plt.figure()
-    ax1 = fig.add_subplot(111)
-    ax1.plot(deadlines, wins, '.b-', label='Percentage of wins over '+str(n_games)+' games')
-    ax1.legend()
-    plt.title('Number of wins for increasing deadlines with discounted rewards, lambda = 29/30')
-    plt.plot(deadlines, wins)
+    lambdas = np.arange(1, 30, 1)/ 30
+    wins = []
+    for lambd in lambdas:
+        deadlines = [100000000]
+        n_games = 1000
+        n_wins = test_geometric(deadlines, n_games, lambd)[0]
+        print(""+str(n_wins)+" for lambda = "+str(lambd))
+        wins.append(n_wins)
+
+
+    plt.plot(lambdas, wins, '.b-', label='Percentage of wins over '+str(n_games)+' games')
+    plt.ylim(0, 1)
+    plt.legend(prop={'size': 15})
+    plt.title('Number of wins for decreasing values of lambda')
     plt.show()
 
